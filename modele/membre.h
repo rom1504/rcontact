1c1372104e3a61309aba3b2815447a88ed4445c4
Vultrao <thomas.lourseyre@gmail.com>
2013-04-02 14:30:15 +0200

    Fin de la documentation

--- modele/membre.h
+++ modele/membre.h
@@ -7,12 +7,22 @@
 #define MEMBRE_H
 #include "structure.h"
 
+/**
+ * @brief The Membre class
+ */
 class Membre : public Structure
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructeur par défaut
+     */
     explicit Membre(QObject *parent = 0);
 
+    /**
+     * @brief toVCard convertis l'objet en format vCard
+     * @return Une QString contenant la conversion
+     */
     QString toVCard() const;
     
 signals:
--- vue/structureedit.h
+++ vue/structureedit.h
@@ -14,23 +14,51 @@ namespace Ui {
 class StructureEdit;
 }
 
+/**
+ * @brief La classe StructureEdit est un widget permettant d'éditer une structure
+ */
 class StructureEdit : public QWidget
 {
     Q_OBJECT
     Q_PROPERTY(Structure* structure READ structure WRITE setStructure USER true)
     
 public:
+    /**
+     * @brief Constructeur par défaut
+     */
     explicit StructureEdit(QWidget *parent = 0);
+    /**
+     * @brief structure renvoie la Structure associé au widget
+     * @return La structure associé au widget
+     */
     Structure * structure() const;
+    /**
+     * @brief setStructure est un setter pour la Structure associée au widget
+     * @param s La nouvelle Structure
+     */
     void setStructure(Structure *s);
     ~StructureEdit();
+    /**
+     * @brief sizeHint taille du widget
+     * @return Un QSize contenant la taille du widget
+     */
     QSize sizeHint() const;
 
 private slots:
+    /**
+     * @brief redimensionner ajuste automatiquement la taille d'une ligne de la structure (pour l'adapter à l'édition)
+     * @param index L'index de la ligne à redimensionner
+     */
     void redimensionner(QModelIndex index);
     
 private:
+    /**
+     * @brief ui est la user interface
+     */
     Ui::StructureEdit *ui;
+    /**
+     * @brief mStructure est la Structure représentée par ce widget
+     */
     Structure * mStructure;
 };
 

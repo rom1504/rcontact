504022314acd1ad71cd25301c4c4c78e4e01615b
rom1504 <romain.rom1@gmail.com>
2013-04-02 04:33:45 +0200

    remise du chargement d'une liste de contacts par défaut.

--- controleur/controleur.cpp
+++ controleur/controleur.cpp
@@ -28,7 +28,7 @@ Controleur::Controleur(QObject *parent) :
     connect(&mVue,SIGNAL(rechercherContacts(Search *)),this,SLOT(rechercherContacts(Search *)));
     connect(&mVue,SIGNAL(finirLaRecherche()),this,SLOT(finirLaRecherche()));
     connect(&mVue,SIGNAL(enregistrerContacts(QString,QString)),mContacts,SLOT(enregistrer(QString,QString)));
-   // modeleListeContacts->charger("exemple.vcf","vCard");
+   modeleListeContacts->charger("exemple.vcf","vCard");
 
     // exemples (voués à disparaitre grâce à l'ajout et à l'import)
 //    Texte * valeur1=new Texte("valeur1");

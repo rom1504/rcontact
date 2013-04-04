246c6d4ea0ad0c6ac1052bc2d7b6f285e02db45f
rom1504 <romain.rom1@gmail.com>
2013-04-04 19:44:35 +0200

    début d'amélioration pour les adresses (pas de modification de l'adresse si pas de modification de la chaine) : permet d'éditer en mode structure sans etre géné par l'autre mode

--- modele/adresse.cpp
+++ modele/adresse.cpp
@@ -21,17 +21,21 @@ QString Adresse::toVCard() const
 
 bool Adresse::fromString(const QString s)
 {
-    QStringList l=s.split(" ");
-    vider();
+    if(toString()!=s)
+    {
+        QStringList l=s.split(" ");
+        vider();
 
-    // à améliorer ou bien repasser en juste Structure ?
-    ajouterChamp(tr("rue"),new Texte(s));
-    ajouterChamp(tr("type"),new Enum("work"));
-    ajouterChamp(tr("zipcode"),new Texte(""));
-    ajouterChamp(tr("localité"),new Texte(""));
-    ajouterChamp(tr("Région"),new Texte(""));
-    ajouterChamp(tr("pays"),new Texte(""));
-    ajouterChamp(tr("geo"),new Loc());
-    return true;
+        // à améliorer ou bien repasser en juste Structure ?
+        ajouterChamp(tr("rue"),new Texte(s));
+        ajouterChamp(tr("type"),new Enum("work"));
+        ajouterChamp(tr("zipcode"),new Texte(""));
+        ajouterChamp(tr("localité"),new Texte(""));
+        ajouterChamp(tr("Région"),new Texte(""));
+        ajouterChamp(tr("pays"),new Texte(""));
+        ajouterChamp(tr("geo"),new Loc());
+        return true;
+    }
+    return false;
 }
 
